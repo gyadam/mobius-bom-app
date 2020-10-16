4149f635b7af7758b3dcdcad289c18853eb2d64d
adam-ubuntu <gy.adam90@gmail.com>
2020-10-16 09:13:51 -0700

    Initial commit with mock server and table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { createServer, Model } from "miragejs";
+const mockData = require('./mock-data.json');
+
+// Mock server for API calls with Mirage JS
+createServer({
+
+  models: {
+    bomItem: Model
+  },
+
+  seeds(server) {
+    server.schema.bomItems.create(mockData.bom[0]);
+    server.schema.bomItems.create(mockData.bom[1]);
+    server.schema.bomItems.create(mockData.bom[2]);
+    server.schema.bomItems.create(mockData.bom[3]);
+  },
+
+  routes() {
+    this.namespace = "mobiusmaterials.com/api/v1";
+
+    this.get("/bom/1001", (schema, request) => {
+      return schema.bomItems.all();
+    });
+
+    this.put("/bom/1001/bomitem/:id", (schema, request) => {
+      let pk = request.params.id;
+      let attrs = JSON.parse(request.requestBody);
+      let bomitem = schema.bomItems.findBy({
+        pk: pk
+      });
+      return bomitem.update(attrs);
+    });
+  },
+})
+
 
 function App() {
+  let [bom, setBom] = useState([]);
+
+  useEffect(() => {
+    // example PUT request
+    // fetch("mobiusmaterials.com/api/v1/bom/1001/bomitem/10001", {
+    //   method: 'PUT',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   pk: 10001,
+    //   body: JSON.stringify(
+    //       {
+    //         "model":"core.bomitem",
+    //         "pk":10001,
+    //         "fields":{
+    //            "uuid":"0b1ee8c4-03bd-4fd8-a016-226dba25f0f6",
+    //            "created_at":"2020-08-27T00:38:01.689Z",
+    //            "updated_at":"2020-08-27T00:38:01.689Z",
+    //            "is_active":true,
+    //            "bom":1001,
+    //            "quantity":7,
+    //            "specific_part":10004,
+    //            "item_unit_cost":"0.3000"
+    //         }
+    //      }
+    //   ),
+    // })
+    // .then
+    (
+      fetch("mobiusmaterials.com/api/v1/bom/1001")
+      .then((response) => response.json())
+      .then((json) => setBom(json.bomItems))
+    )  
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Part</th>
+          <th>Quantity</th>
+          <th>Item unit cost</th>
+        </tr>
+        {bom.map(item => (
+          <tr key={item.pk}>
+            <td>{item.fields.specific_part}</td>
+            <td>{item.fields.quantity}</td>
+            <td>{item.fields.item_unit_cost}</td>
+          </tr>
+        ))}
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
